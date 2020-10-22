e8c8d03ce8810f899388abaa9a65986122cb89cb
sai krishna <saikrishna.v268@gmail.com>
2020-10-22 14:10:02 -0400

    Nova scotia

--- File 12.php
+++ File 12.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World. This is Jonas Fitness INC. This is from Halifax</p>'; ?> 
+ <?php echo '<p>Hello World. This is Jonas Fitness INC. This is from Halifax. Nova scotia</p>'; ?> 
  </body>
 </html>
